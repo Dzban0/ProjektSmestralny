f873f4567b0b4cffa352f19e2a5c7c2e9c738f9c
MrKazio <115210775+Dzban0@users.noreply.github.com>
2023-05-22 21:35:42 +0200

    Change 0.11

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -42,5 +42,94 @@ namespace ProjektSmestralny
         {
 
         }
+        public static partial class MyConsoleApp
+        {
+            private static bool shouldContinue = true;
+            private static int nextID = 0;
+            private static List<RaceMember> raceMembers = new List<RaceMember>();
+            public static void Start()
+            {
+                StartupMessage();
+                SampleDataFill();
+
+                while (shouldContinue)
+                {
+                    Analyze(Console.ReadLine());
+                }
+
+                ExitMessage();
+            }
+
+            private static void Analyze(string? instruction)
+            {
+                if (instruction is null || instruction == "") return;
+
+                string command = instruction.Split(' ')[0].ToUpper();
+                switch (command)
+                {
+                    case "ADD":
+                        ADD(instruction);
+                        break;
+                    case "DEL":
+                        DEL(instruction);
+                        break;
+                    case "CMP":
+                        CMP(instruction);
+                        break;
+                    case "LST":
+                        LST();
+                        break;
+                    case "CLS":
+                        CLS();
+                        break;
+                    default:
+                        NotRecognized(command);
+                        break;
+                }
+            }
+
+            private static void StartupMessage()
+            {
+                ConsoleColor consoleColor = Console.ForegroundColor; // previous color
+                StringBuilder sb = new StringBuilder();
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                sb.AppendLine("Welcome!");
+                sb.AppendLine("In this app you can add Marathon members and compare their times.");
+                sb.AppendLine("To do so, you can use the following commands:");
+                sb.AppendLine();
+                Console.WriteLine(sb);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                sb.AppendLine("ADD <Name> <Surname> <Time hh:mm:ss> - adds new entry with a new member");
+                sb.AppendLine("DEL <ID> - remove a race member");
+                sb.AppendLine("CMP <ID> <ID> - compares two race members");
+                sb.AppendLine("LST - list all race members");
+                sb.AppendLine("CLS - close the program");
+                sb.AppendLine();
+                Console.WriteLine(sb);
+
+                Console.ForegroundColor = consoleColor;
+            }
+
+            private static void ExitMessage()
+            {
+                ConsoleColor consoleColor = Console.ForegroundColor; // previous color
+                StringBuilder sb = new StringBuilder();
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                sb.AppendLine("Quitting...");
+                sb.AppendLine();
+                Console.WriteLine(sb);
+
+                Console.ForegroundColor = consoleColor;
+            }
+
+            private static int NextMovie()
+            {
+                nextMovie++;
+                return nextMovie;
+            }
+        }
     }
 }
