e731bdd9c6257ac54150877af993a8ccec5b1878
MrKazio <115210775+Dzban0@users.noreply.github.com>
2023-06-11 19:23:26 +0200

    DesignChange 1

---
+++ Model/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ProjektSmestralny.Core;
+using ProjektSmestralny.Model.ViewModel;
+using ProjektSmestralny.View;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSmestralny
+{
+    public class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVm { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+        }
+
+
+
+        // public static string? Films { get; set; }
+        // public static string? Actors { get; set; }
+        // public static string? Movie_Studio { get; set; }
+        // public static string? Category { get; set; }
+
+    }
+}
