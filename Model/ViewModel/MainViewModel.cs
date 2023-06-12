2fc350c12b3f628c72156110753e9596417305ca
MrKazio <115210775+Dzban0@users.noreply.github.com>
2023-06-12 17:03:44 +0200

    DesignChange 1.01

--- Model/ViewModel/MainViewModel.cs
+++ Model/ViewModel/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using ProjektSmestralny.Core;
 using ProjektSmestralny.Model.ViewModel;
-using ProjektSmestralny.View;
+// using ProjektSmestralny.View;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,22 +12,22 @@ namespace ProjektSmestralny.Model.ViewModel
 {
     public class MainViewModel
     {
-        public HomeViewModel HomeVm { get; set; }
-        private object _currentView;
+        // public HomeViewModel HomeVm { get; set; }
+        // private object _currentView;
 
-        public object CurrentView
-        {
-            get { return _currentView; }
-            set
-            {
-                _currentView = value;
-            }
-        }
-        public MainViewModel()
-        {
-            HomeVm = new HomeViewModel();
-            CurrentView = HomeVm;
-        }
+        // public object CurrentView
+        //     {
+        //         get { return _currentView; }
+        //         set
+        //     {
+        //         _currentView = value;
+        //     }
+        // }
+        // public MainViewModel()
+        // {
+        // HomeVm = new HomeViewModel();
+        // CurrentView = HomeVm;
+        // }
 
         // public static string? Films { get; set; }
         // public static string? Actors { get; set; }
